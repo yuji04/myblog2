31bd00a89363a151edcd5a600ce501926460f4d6
Yuji Yamaaguchi <04yuji.yamaguchi@gmail.com>
2017-02-18 08:08:34 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'message#index'
   resources :test_ms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
